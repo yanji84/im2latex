c2d88c810e6bfca26067365308ad61c5a7449db2
Ji Yan <jiyan@Jis-MacBook-Air.local>
2016-09-11 14:29:37 -0700

    test on real dataset

--- Im2latex.py
+++ Im2latex.py
@@ -1,7 +1,5 @@
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
-import tflearn.datasets.mnist as mnist
 from Globals import *
 from ConvNet import *
 from DataLoader import *
@@ -15,10 +13,23 @@ learning_rate = constants['learning_rate']
 batch_size = constants['batch_size']
 
 # Parameters
-training_iters = 200000
+training_iters = 20000000
 display_step = 10
 test_step = 10
 dropout = 0.75
+
+def setup_summaries():
+    loss = tf.Variable(0.)
+    tf.scalar_summary("Loss", loss)
+    train_acc = tf.Variable(0.)
+    tf.scalar_summary("Train Accuracy", train_acc)
+    test_acc = tf.Variable(0.)
+    tf.scalar_summary("Test Accuracy", test_acc)
+    summary_vars = [loss, train_acc, test_acc]
+    summary_placeholders = [tf.placeholder("float") for i in range(len(summary_vars))]
+    update_ops = [summary_vars[i].assign(summary_placeholders[i]) for i in range(len(summary_vars))]
+    summary_op = tf.merge_all_summaries()
+    return summary_placeholders, update_ops, summary_op
 
 def calculate_accuracy(samples, batch_x):
     step = 0.0
@@ -46,6 +57,7 @@ def train_digit_sequence():
 
     init = tf.initialize_all_variables()
     saver = tf.train.Saver()
+    summary_placeholders, update_ops, summary_op = setup_summaries()
 
     clasDict = {}
     clasDict['#'] = 0
@@ -60,11 +72,19 @@ def train_digit_sequence():
     clasDict['8'] = 9
     clasDict['9'] = 10
     clasDict['*'] = 11
-    dataLoader = DataLoader("/Users/jiyan/Downloads/train/", image_dim, clasDict, n_classes, n_steps, debug=True)
+    dataLoader = DataLoader("/Users/jiyan/Downloads/train/", image_dim, clasDict, n_classes, n_steps, debug=False)
 
     # Launch the graph
     with tf.Session() as sess:
         sess.run(init)
+
+        writer = tf.train.SummaryWriter("/tmp/im2latex_logs", sess.graph)
+
+        checkpoint = tf.train.get_checkpoint_state("/tmp")
+        if checkpoint and checkpoint.model_checkpoint_path:
+            saver.restore(sess, checkpoint.model_checkpoint_path)
+            print "successfully loaded checkpoint"
+
         step = 1
         # Keep training until reach max iterations
         while step * batch_size < training_iters:
@@ -73,19 +93,27 @@ def train_digit_sequence():
             sess.run(optimizer, feed_dict={x: batch_x, y: batch_y,
                                            image: images, mask: masks})
             if step % display_step == 0:
-                # Calculate batch loss and accuracy
                 loss,training_generated = sess.run([cost, generated], feed_dict={x: batch_x, y: batch_y,
                                            image: images, mask: masks})
-                acc = calculate_accuracy(training_generated, batch_x)
-                print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + str(loss) + ", training acc=" + str(acc))
+                train_acc = calculate_accuracy(training_generated, batch_x)
+                print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + str(loss) + ", training acc=" + str(train_acc))
 
-            if step % test_step == 0:
-                batch_x, batch_y, images, masks = dataLoader.next_test_batch(batch_size)
-                samples = sess.run(generated, feed_dict={x: batch_x, y: batch_y,
-                                                   image: images, mask: masks})
-                acc = calculate_accuracy(samples, batch_x)
-                print("Iter " + str(step*batch_size) + ", Test Accuracy= " + str(acc))
+                test_batch_x, test_batch_y, test_images, test_masks = dataLoader.next_test_batch(batch_size)
+                samples = sess.run(generated, feed_dict={x: test_batch_x, y: test_batch_y,
+                                                   image: test_images, mask: test_masks})
+                test_acc = calculate_accuracy(samples, test_batch_x)
+                print("Iter " + str(step*batch_size) + ", Test Accuracy= " + str(test_acc))
+
+                save_path = saver.save(sess, "/tmp/im2latex.ckpt")
+                print("Model saved in file: %s" % save_path)
+
+                sess.run(update_ops[0], feed_dict={summary_placeholders[0]:float(loss)})
+                sess.run(update_ops[1], feed_dict={summary_placeholders[1]:float(train_acc)})
+                sess.run(update_ops[2], feed_dict={summary_placeholders[2]:float(test_acc)})
+                summary_str = sess.run(summary_op)
+                writer.add_summary(summary_str, step)
             step += 1
+
         print("Optimization Finished!")
 
 train_digit_sequence()
