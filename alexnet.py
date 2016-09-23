f38d85ce300f5cbcf19c63f61edebe44131465b7
root <root@deeplearning-node1.ifjcxpun4zoutjwre20dz0brid.gx.internal.cloudapp.net>
2016-09-23 03:43:02 +0000

    fix alexnet

--- alexnet.py
+++ alexnet.py
@@ -7,7 +7,7 @@ class alexnet:
                 'wc1': tf.Variable(tf.random_normal([3, 3, 1, 64])),
                 'wc2': tf.Variable(tf.random_normal([3, 3, 64, 128])),
                 'wc3': tf.Variable(tf.random_normal([3, 3, 128, 256])),
-                'wd1': tf.Variable(tf.random_normal([4, 4, 256, 1024])),
+                'wd1': tf.Variable(tf.random_normal([4*4*256, 1024])),
                 'wd2': tf.Variable(tf.random_normal([1024, 1024])),
                 'out': tf.Variable(tf.random_normal([1024, 10]))
             }
@@ -17,25 +17,25 @@ class alexnet:
                 'bc3': tf.Variable(tf.random_normal([256])),
                 'bd1': tf.Variable(tf.random_normal([1024])),
                 'bd2': tf.Variable(tf.random_normal([1024])),
-                'out': tf.Variable(tf.random_normal([n_classes]))
+                'out': tf.Variable(tf.random_normal([10]))
             }
 
         dropout = 0.85
         imgs = tf.reshape(imgs, shape=[-1, 28, 28, 1])
 
-        conv1 = conv2d('conv1', imgs, weights['wc1'], biases['bc1'])
-        pool1 = max_pool('pool1', conv1, k=2)
-        norm1 = norm('norm1', pool1, lsize=4)
+        conv1 = self.conv2d('conv1', imgs, weights['wc1'], biases['bc1'])
+        pool1 = self.max_pool('pool1', conv1, k=2)
+        norm1 = self.norm('norm1', pool1, lsize=4)
         norm1 = tf.nn.dropout(norm1, dropout)
 
-        conv2 = conv2d('conv2', norm1, weights['wc2'], biases['bc2'])
-        pool2 = max_pool('pool2', conv2, k=2)
-        norm2 = norm('norm2', pool2, lsize=4)
+        conv2 = self.conv2d('conv2', norm1, weights['wc2'], biases['bc2'])
+        pool2 = self.max_pool('pool2', conv2, k=2)
+        norm2 = self.norm('norm2', pool2, lsize=4)
         norm2 = tf.nn.dropout(norm2, dropout)
 
-        conv3 = conv2d('conv3', norm2, weights['wc3'], biases['bc3'])
-        pool3 = max_pool('pool3', conv3, k=2)
-        norm3 = norm('norm3', pool3, lsize=4)
+        conv3 = self.conv2d('conv3', norm2, weights['wc3'], biases['bc3'])
+        pool3 = self.max_pool('pool3', conv3, k=2)
+        norm3 = self.norm('norm3', pool3, lsize=4)
         norm3 = tf.nn.dropout(norm3, dropout)
 
         dense1 = tf.reshape(norm3, [-1, weights['wd1'].get_shape().as_list()[0]]) 
@@ -43,19 +43,16 @@ class alexnet:
         dense2 = tf.nn.relu(tf.matmul(dense1, weights['wd2']) + biases['bd2'], name='fc2') # Relu activation
         self.logits = tf.matmul(dense2, weights['out']) + biases['out']
         self.labels = labels
-
-    def forward(self):
         self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.logits, self.labels))
         self.optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.cost)
-        
-        self.correct_pred = tf.equal(tf.argmax(self.fc3l, 1), tf.argmax(labels, 1))
+        self.correct_pred = tf.equal(tf.argmax(self.logits, 1), tf.argmax(labels, 1))
         self.accuracy = tf.reduce_mean(tf.cast(self.correct_pred, tf.float32))
 
-    def conv2d(name, l_input, w, b):
+    def conv2d(self, name, l_input, w, b):
         return tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(l_input, w, strides=[1, 1, 1, 1], padding='SAME'),b), name=name)
 
-    def max_pool(name, l_input, k):
+    def max_pool(self, name, l_input, k):
         return tf.nn.max_pool(l_input, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME', name=name)
 
-    def norm(name, l_input, lsize=4):
+    def norm(self, name, l_input, lsize=4):
         return tf.nn.lrn(l_input, lsize, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name=name)
