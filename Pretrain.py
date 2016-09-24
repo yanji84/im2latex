c2fb3aaefe15303690b0da09899666c718f08c6f
Ji Yan <jiyan@Jis-MacBook-Air.local>
2016-09-24 15:59:32 -0700

    adapt tensorflow cifar model

--- Pretrain.py
+++ Pretrain.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import cv2
+import math
 from vgg16 import *
 from alexnet import *
 
@@ -14,8 +15,8 @@ display_step = 1
 idx = 0
 step = 0
 
-#imagePath = "/Users/jiyan/Desktop/class/"
-imagePath = "/home/deeplearningdev/class/"
+imagePath = "/Users/jiyan/Desktop/class/"
+#imagePath = "/home/deeplearningdev/class/"
 
 def load():
   global onehotLabels
@@ -25,8 +26,15 @@ def load():
       parts = line.split(",")
       fileName = imagePath + parts[0]
       print fileName
-      img = cv2.resize(cv2.imread(fileName, cv2.IMREAD_UNCHANGED), (224, 224))
-      allImages.append(img)
+      img = cv2.resize(cv2.imread(fileName, cv2.IMREAD_UNCHANGED), (32, 32))
+
+      # white wash image
+      mn = np.mean(img)
+      y = img.astype(np.float32)
+      y -= mn
+      img_afterww = y
+
+      allImages.append(img_afterww)
       allLabels.append(parts[1])
       if debug and len(allLabels) > 10:
         break
@@ -51,19 +59,8 @@ def next(size):
       idx = 0
   return images,ys
 
-def setup_summaries():
-  loss = tf.Variable(0.)
-  tf.scalar_summary("Loss", loss)
-  train_acc = tf.Variable(0.)
-  tf.scalar_summary("Train Accuracy", train_acc)
-  summary_vars = [loss, train_acc]
-  summary_placeholders = [tf.placeholder("float") for i in range(len(summary_vars))]
-  update_ops = [summary_vars[i].assign(summary_placeholders[i]) for i in range(len(summary_vars))]
-  summary_op = tf.merge_all_summaries()
-  return summary_placeholders, update_ops, summary_op
-
 if __name__ == '__main__':
-  x = tf.placeholder(tf.float32, [None, 224, 224, 3])
+  x = tf.placeholder(tf.float32, [None, 32, 32, 3])
   y = tf.placeholder(tf.float32, [None, 10])
 
   load()
@@ -72,8 +69,7 @@ if __name__ == '__main__':
   
   init = tf.initialize_all_variables()
   saver = tf.train.Saver()
-  summary_placeholders, update_ops, summary_op = setup_summaries()
-
+  summary_op = tf.merge_all_summaries()
   with tf.Session() as sess:
       sess.run(init)
       writer = tf.train.SummaryWriter("vgglogs", sess.graph)
@@ -91,21 +87,14 @@ if __name__ == '__main__':
           sess.run(cnn.optimizer, feed_dict={x: images, y: ys})
           if step % display_step == 0:
               # Calculate batch loss and accuracy
-              loss, acc, logits = sess.run([cnn.cost,cnn.accuracy,cnn.logits], feed_dict={x: images,
+              loss, acc, summary_str = sess.run([cnn.cost,cnn.accuracy,summary_op], feed_dict={x: images,
                                                                 y: ys})
               print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
                     "{:.6f}".format(loss) + ", Training Accuracy= " + \
                     "{:.5f}".format(acc))
               
-              print logits
-              print ys
-
               save_path = saver.save(sess, "vggmodel/im2latex.ckpt")
               print("Model saved in file: %s" % save_path)
-
-              sess.run(update_ops[0], feed_dict={summary_placeholders[0]:float(loss)})
-              sess.run(update_ops[1], feed_dict={summary_placeholders[1]:float(acc)})
-              summary_str = sess.run(summary_op)
               writer.add_summary(summary_str, step)
           step += 1
       print("Optimization Finished!")
