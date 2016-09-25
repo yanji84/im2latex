70f1c7c8efedbd1f48ad5acde434788225a91085
Ji Yan <jiyan@Jis-MacBook-Air.hsd1.wa.comcast.net>
2016-09-25 11:36:45 -0700

    remove conv vis for conv2 layer

--- SvhnNet.py
+++ SvhnNet.py
@@ -41,14 +41,6 @@ class SvhnNet:
         bias = tf.nn.bias_add(conv, biases)
         conv2 = tf.nn.relu(bias, name=scope.name)
         self.activation_summary(conv2)
-
-        with tf.variable_scope('visualization'):
-            x_min = tf.reduce_min(kernel)
-            x_max = tf.reduce_max(kernel)
-            kernel_0_to_1 = (kernel - x_min) / (x_max - x_min)
-            kernel_transposed = tf.transpose (kernel_0_to_1, [3, 0, 1, 2])
-            tf.image_summary('conv2/filters', kernel_transposed, max_images=64)
-
     
       # norm2
       norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
